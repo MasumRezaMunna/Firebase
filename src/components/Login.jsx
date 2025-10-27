92f15587739d699f3959d07980aa543a5d500e00
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-28 02:19:44 +0600

    Display logged-in user info, Sign out, toggle sign in, sign out

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,55 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
-import { auth } from '../firebase/firebase.init';
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase.init";
 
 const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
+  const [user, setUser] = useState(null);
 
-    const handleGoogleSignIn = () =>{
-        // console.log('google button clicked')
-        signInWithPopup(auth, googleProvider)
-        .then(result => {
-            console.log(result.user)
-        })
-        .catch(error =>{
-            console.log(error)
-        })
-    }
+  const handleGoogleSignIn = () => {
+    // console.log('google button clicked')
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign Out Done");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Please Login</h2>
+      {/* <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+      <button onClick={handleSignOut}>Sign Out</button> */}
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+      )}
+
+      {user && (
         <div>
-            <h2>Please Login</h2>
-            <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+          <h3>{user.displayName}</h3>
+          <h5>Email: {user.email}</h5>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
