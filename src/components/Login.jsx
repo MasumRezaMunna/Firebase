d1dfb3a5efe07f9d52ae876de12adbf820db5162
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-28 01:11:24 +0600

    added signin pop up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../firebase/firebase.init';
+
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
+
+    const handleGoogleSignIn = () =>{
+        // console.log('google button clicked')
+        signInWithPopup(auth, googleProvider)
+        .then(result => {
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
     return (
         <div>
             <h2>Please Login</h2>
+            <button onClick={handleGoogleSignIn}>Sign In With Google</button>
         </div>
     );
 };
