3a0553cfa9ba1ee91ed0ed221408f80c1094cdd3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-27 23:25:33 +0600

    firebase installed

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrefMM9qWUU7wfhl6iQMKJ1QsY0IatSAc",
+  authDomain: "simple-firebase-auth-5efad.firebaseapp.com",
+  projectId: "simple-firebase-auth-5efad",
+  storageBucket: "simple-firebase-auth-5efad.firebasestorage.app",
+  messagingSenderId: "357355073589",
+  appId: "1:357355073589:web:bcd6ec5e52c87bf13b817c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Root from './layout/Root.jsx';
 import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home
+      },
+      {
+        path: 'login',
+        Component: Login
       }
     ]
   },
